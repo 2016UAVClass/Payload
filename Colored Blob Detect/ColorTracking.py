9ac8301ae8f166e2f1c8d1ea3ba1fb6df6e585e0
Richard Kerr <richard.m.kerr@vanderbilt.edu>
2016-02-24 22:25:49 -0600

    Sorta fixed the blob detector?

--- Colored Blob Detect/ColorTracking.py
+++ Colored Blob Detect/ColorTracking.py
@@ -4,22 +4,18 @@ import numpy as np
 cap = cv2.VideoCapture(0)
 
 blob_detect_config = cv2.SimpleBlobDetector_Params()
-
-
-# Change thresholds
-# blob_detect_config.minThreshold = 10
-# blob_detect_config.maxThreshold = 200
-
+blob_detect = cv2.SimpleBlobDetector(blob_detect_config)
 def regenerate_blob_detector(x):
-    global blob_detect
     # Filter by Area.
+    print("Called")
+
     blob_detect_config.filterByArea = True
     blob_detect_config.minArea = cv2.getTrackbarPos('Area_Min', 'image')
     blob_detect_config.maxArea = cv2.getTrackbarPos('Area_Max', 'image')
 
     # Filter by Circularity
     blob_detect_config.filterByCircularity = False
-    blob_detect_config.minCircularity = 0.2
+    blob_detect_config.minCircularity = 0.8
 
     # Filter by Convexity
     blob_detect_config.filterByConvexity = False
@@ -29,7 +25,7 @@ def regenerate_blob_detector(x):
     blob_detect_config.filterByInertia = False
     blob_detect_config.minInertiaRatio = 0.01
 
-    blob_detect = cv2.SimpleBlobDetector(blob_detect_config)
+    print('Finished')
 
 
 cv2.namedWindow('frame')
@@ -46,11 +42,13 @@ while (1):
     frame_ycrcb = cv2.cvtColor(frame, cv2.COLOR_BGR2YCR_CB)
     frame_cr = cv2.extractChannel(frame_ycrcb, 1)
 
-    _, frame_cr_highlights = cv2.threshold(frame_cr, 135, 255, cv2.THRESH_BINARY)
+    _, frame_cr_highlights = cv2.threshold(frame_cr, 135, 255, cv2.THRESH_BINARY_INV)
     # Threshold the HSV image to get only blue colors
     # mask = cv2.inRange(hsv, lower_green, upper_green)
 
     keypoints = blob_detect.detect(frame_cr_highlights)
+
+    print("Found " + str(len(keypoints)) + " keypoints")
 
     im_with_keypoints = cv2.drawKeypoints(frame_cr_highlights, keypoints, np.array([]), (0, 255, 0),
                                           cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
