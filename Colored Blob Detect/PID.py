d9f1520e24c45fc3aeba50430f5d37729c097d29
Richard Kerr <richard.m.kerr@vanderbilt.edu>
2016-02-28 14:06:36 -0600

    Wrote custom PID controlelr code. Does it work? Who knows...

--- Colored Blob Detect/PID.py
+++ Colored Blob Detect/PID.py
@@ -1,76 +1,52 @@
-#-------------------------------------------------------------------------------
-# PID.py
-# A simple implementation of a PID controller
-#-------------------------------------------------------------------------------
-# Example source code for the book "Real-World Instrumentation with Python"
-# by J. M. Hughes, published by O'Reilly Media, December 2010,
-# ISBN 978-0-596-80956-0.
-#-------------------------------------------------------------------------------
-
 import time
 
+
 class PID:
-    """ Simple PID control.
 
-        This class implements a simplistic PID control algorithm. When first
-        instantiated all the gain variables are set to zero, so calling
-        the method GenOut will just return zero.
-    """
     def __init__(self):
-        # initialze gains
-        self.Kp = 0
-        self.Kd = 0
-        self.Ki = 0
+        self.kp = 0
+        self.ki = 0
+        self.kd = 0
+        self.accumulated_error = 0
+        self.acceptable_error = 0
+        self.accumulated_max = 0
+        self.prev_time = 0
+        self.prev_error = None
 
-        self.Initialize()
+    def set_kp(self, kp):
+        self.kp = kp
 
-    def SetKp(self, invar):
-        """ Set proportional gain. """
-        self.Kp = invar
+    def set_ki(self, ki):
+        self.ki = ki
 
-    def SetKi(self, invar):
-        """ Set integral gain. """
-        self.Ki = invar
+    def set_kd(self, kd):
+        self.kd = kd
 
-    def SetKd(self, invar):
-        """ Set derivative gain. """
-        self.Kd = invar
+    def set_acceptable_error(self, error):
+        self.acceptable_error = error
 
-    def SetPrevErr(self, preverr):
-        """ Set previous error value. """
-        self.prev_err = preverr
+    def set_accumulated_error_max(self, error):
+        self.accumulated_max = error
 
-    def Initialize(self):
-        # initialize delta t variables
-        self.currtm = time.time()
-        self.prevtm = self.currtm
+    def out(self, error):
+        # P
+        p = self.kp * error
 
-        self.prev_err = 0
+        # D
+        current_time = time.time()
+        if self.prev_error is not None and self.time != 0:
+            error_delta = error - self.prev_error
+            time_delta = self.prev_time - current_time
+            d = (error_delta/time_delta * self.ki)
+        else:
+            d = 0
 
-        # term result variables
-        self.Cp = 0
-        self.Ci = 0
-        self.Cd = 0
+        # I factor
+        self.accumulated_error += error
+        if error <= self.acceptable_error:
+            self.accumulated_error = 0
+        if self.accumulated_error > self.accumulated_max:
+            self.acceptable_error = self.accumulated_max
+        i = self.accumulated_error * self.ki
 
-
-    def GenOut(self, error):
-        """ Performs a PID computation and returns a control value based on
-            the elapsed time (dt) and the error signal from a summing junction
-            (the error parameter).
-        """
-        self.currtm = time.time()               # get t
-        dt = self.currtm - self.prevtm          # get delta t
-        de = error - self.prev_err              # get delta error
-
-        self.Cp = self.Kp * error               # proportional term
-        self.Ci += error * dt                   # integral term
-
-        self.Cd = 0
-        if dt > 0:                              # no div by zero
-            self.Cd = de/dt                     # derivative term
-
-        self.prevtm = self.currtm               # save t for next pass
-        self.prev_err = error                   # save t-1 error
-
-        # sum the terms and return the result
-        return self.Cp + (self.Ki * self.Ci) + (self.Kd * self.Cd)
+        return p + i + d
